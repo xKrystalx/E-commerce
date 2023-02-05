3fc5b4446864a88c22494aa3b2f4dd5f3a38ed54
xKrystalx <alek8061@gmail.com>
2023-02-05 22:02:16 +0100

    dotpay env variable

--- api/controllers/CartController.js
+++ api/controllers/CartController.js
@@ -5,6 +5,8 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const { dotpayID } = require('../../config/local');
+
 module.exports = {
     addToCart: async function(req, res){ 
         if(req.session.cart){
@@ -41,7 +43,7 @@ module.exports = {
                 if(err) return err;
                 //DotPay requires a sha256 checksum that consists of parameters sent in the url and a few others
                 var shajs = require('sha.js');
-                var digest = shajs('sha256').update('hmfhwDaT6b2pStGUMuG8GLWSNSBMJ3zr722484'+req.session.cart.totalPrice+'PLN'+order.id+'http://localhost:1337/order_completed/'+order.id+'0').digest('hex');
+                var digest = shajs('sha256').update(dotpayID + req.session.cart.totalPrice+'PLN'+order.id+'http://localhost:1337/order_completed/'+order.id+'0').digest('hex');
                 return res.redirect('https://ssl.dotpay.pl/test_payment/?id=722484&amount='+req.session.cart.totalPrice+'&currency=PLN&url=http://localhost:1337/order_completed/'+order.id+'&description='+order.id+'&type=0&chk='+digest);
             });
         }
